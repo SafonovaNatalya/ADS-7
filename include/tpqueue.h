33916d553ac157bca0069ba425dbdc8497e9ffd3
SafonovaNatalya <116429612+SafonovaNatalya@users.noreply.github.com>
2023-04-28 10:17:56 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,49 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+#include <list>
 
 struct SYM {
   char ch;
   int prior;
+};
+
+template<typename T>
+class TPQueue {
+  private:
+  std::list <T> spisok;
+  bool helper;
+  T element;
+
+ public:
+  bool isEmpty() const {
+  return spisok.empty();
+  }
+  ~TPQueue() {
+  delete spisok;
+  }
+  void push(const T& value) {
+  if (isEmpty()){
+    spisok.push_back(value);
+  } else {
+    helper = false;
+    for (auto i = spisok.begin(); i != spisok.end(); i++){
+      if ((* i).prior < value.prior){
+        spisok.insert(i, value);
+        helper = true;
+        break;
+      }
+    }
+    if (!helper){
+      spisok.push_back(value);
+    }
+  }
+  }
+  T& pop() {
+  element = spisok.front();
+  spisok.pop_front();
+  return element;
+  }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
